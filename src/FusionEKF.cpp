8eed72afa487ca14a4e1ac30ad04ef6a9e7cefbc
kadirhas <31936408+kadirhas@users.noreply.github.com>
2018-01-13 20:47:50 +0300

    debug

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -92,6 +92,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
     // done initializing, no need to predict or update
     is_initialized_ = true;
+    cout << "this is processed" << endl;
     return;
   }
 
