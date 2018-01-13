aea8ba2a1ada2369d7eed3fa8bbc543da56f16a9
kadirhas <31936408+kadirhas@users.noreply.github.com>
2018-01-13 21:13:27 +0300

    called update functions are switched

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -148,14 +148,14 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.R_ = MatrixXd(3,3);
     ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.R_ = R_radar_;
-    ekf_.Update(measurement_pack.raw_measurements_);
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
     ekf_.H_ = MatrixXd(2,4);
     ekf_.R_ = MatrixXd(2,2);
     ekf_.R_ = R_laser_;
     ekf_.H_ = H_laser_;
-    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
